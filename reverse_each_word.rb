1daeb5763ad92e6608244133999c4719f578130d
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-04-30 13:08:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,10 @@ def reverse_each_word(str)
   #arr = []
   #str.split(" ").each do |ele|
     #arr.push(ele.reverse)
+  #end
+  #arr.join(" ")
+  
+  arr = str.split(" ").collect do |ele|
+    ele.reverse
   end
-  arr.join(" ")
 end
